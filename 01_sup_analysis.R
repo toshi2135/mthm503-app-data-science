b2591371dc48643e3f8281ae1f185142cc054298
Tuan Nguyen <dn370@exeter.ac.uk>
2025-07-14 20:33:32 +0000

    Build Random Forest baseline model

--- 01_sup_analysis.R
+++ 01_sup_analysis.R
@@ -106,3 +106,62 @@ test_data %>%
        x = "Casualty Severity",
        y = "Percentage") +
   theme_minimal()
+
+# Build Random Forest baseline model
+library(ranger)
+rf_rec <- recipe(casualty_severity ~ ., data = train_data)
+rf_spec <- rand_forest(trees = 500) %>%
+  set_engine("ranger") %>%
+  set_mode("classification")
+rf_wf <- workflow() %>%
+  add_recipe(rf_rec) %>%
+  add_model(rf_spec)
+# Fit the model
+rf_fit <- rf_wf %>%
+  fit(data = train_data)
+
+# Check the model
+rf_preds <- predict(rf_fit, test_data, type = "prob") %>%
+  bind_cols(predict(rf_fit, test_data)) %>%
+  bind_cols(test_data)
+rf_metrics <- rf_preds %>%
+  metrics(truth = casualty_severity, estimate = .pred_class)
+rf_roc <- roc_curve(rf_preds, truth = casualty_severity,
+                    .pred_Slight, .pred_Serious, .pred_Fatal)
+autoplot(rf_roc)
+# Check the confusion matrix
+rf_conf_mat <- conf_mat(rf_preds, truth = casualty_severity, estimate = .pred_class)
+rf_conf_mat %>%
+  autoplot(type = "heatmap") +
+  labs(title = "Confusion Matrix for Random Forest Model",
+       x = "Predicted",
+       y = "Actual") +
+  theme_minimal()
+# Check the accuracy
+rf_accuracy <- rf_preds %>%
+  accuracy(truth = casualty_severity, estimate = .pred_class)
+rf_accuracy %>%
+  mutate(accuracy = round(.estimate, 3)) %>%
+  select(-.estimate) %>%
+  knitr::kable(caption = "Random Forest Model Accuracy")
+# Check the precision
+rf_precision <- rf_preds %>%
+  precision(truth = casualty_severity, estimate = .pred_class)
+rf_precision %>%
+  mutate(precision = round(.estimate, 3)) %>%
+  select(-.estimate) %>%
+  knitr::kable(caption = "Random Forest Model Precision")
+# Check the recall
+rf_recall <- rf_preds %>%
+  recall(truth = casualty_severity, estimate = .pred_class)
+rf_recall %>%
+  mutate(recall = round(.estimate, 3)) %>%
+  select(-.estimate) %>%
+  knitr::kable(caption = "Random Forest Model Recall")
+# Check the F1 score
+rf_f1 <- rf_preds %>%
+  f_meas(truth = casualty_severity, estimate = .pred_class)
+rf_f1 %>%
+  mutate(f1 = round(.estimate, 3)) %>%
+  select(-.estimate) %>%
+  knitr::kable(caption = "Random Forest Model F1 Score")
