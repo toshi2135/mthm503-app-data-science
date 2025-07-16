def07d360fcdab07d2b04b6cbc1f1496d914deef
Tuan Nguyen <dn370@exeter.ac.uk>
2025-07-16 23:20:29 +0000

    feat: Update summary into the pipelines

---
+++ R/01_sup_pipeline/sup_summary.R
@@ -0,0 +1,26 @@
+# 01_sup_pipeline/sup_summary.R
+
+sup_summarise <- function() {
+  # Create a summary table with all models
+  sup_summary <- bind_rows(
+    rf_summary,
+    log_summary,
+    rf_summary_weighted,
+    rf_summary_final
+  ) %>%
+    mutate(
+      model = factor(
+        model,
+        levels = c(
+          "Random Forest",
+          "Logistic Regression",
+          "Random Forest with Case Weights",
+          "Final Random Forest"
+        )
+      )
+    ) %>%
+    knitr::kable(
+      caption = "Model Comparison Summary"
+    )
+  sup_summary
+}
