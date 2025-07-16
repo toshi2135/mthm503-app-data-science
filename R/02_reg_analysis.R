a4b5f9dfe485578d5be3bddae97e637d4e234070
Tuan Nguyen <dn370@exeter.ac.uk>
2025-07-16 22:27:29 +0000

    fix: Format to match lintr

--- R/02_reg_analysis.R
+++ R/02_reg_analysis.R
@@ -190,7 +190,8 @@ chisq.test(table_age)
 table_sex <- table(fire_rescue_clean$sex, fire_rescue_clean$extrication)
 chisq.test(table_sex)
 # Chi-squared test to examine the association between age_band and extrication
-chisq_test_result <- chisq.test(table(fire_rescue_clean$age_band, fire_rescue_clean$extrication))
+chisq_test_result <- chisq.test(table(fire_rescue_clean$age_band,
+                                      fire_rescue_clean$extrication))
 
 # Print the results of the Chi-squared test
 print(chisq_test_result)
@@ -227,8 +228,10 @@ gam_2df_tidy <- tidy_vglm(model_gam_test_2df, "Multinomial GAM 2df")
 gam_3df_tidy <- tidy_vglm(model_gam_test_3df, "Multinomial GAM 3df")
 
 reg_model_summary <- bind_rows(
-  tidy(model_multi, conf.int = TRUE, exponentiate = TRUE) %>% mutate(model = "Multinomial GLM"),
-  tidy(model_multi2, conf.int = TRUE, exponentiate = TRUE) %>% mutate(model = "Updated Multinomial GLM"),
+  tidy(model_multi, conf.int = TRUE, exponentiate = TRUE) %>%
+    mutate(model = "Multinomial GLM"),
+  tidy(model_multi2, conf.int = TRUE, exponentiate = TRUE) %>%
+    mutate(model = "Updated Multinomial GLM"),
   binary_models_df %>% mutate(model = "Binomial Regression"),
   gam_2df_tidy,
   gam_3df_tidy
