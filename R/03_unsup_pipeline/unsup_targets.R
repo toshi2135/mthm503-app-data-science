acf13fb8c439857728976fd7d5a77d4b1fa0a739
Tuan Nguyen <dn370@exeter.ac.uk>
2025-07-17 16:07:24 +0000

    init: Initialise the Unsupervised Learning pipeline

---
+++ R/03_unsup_pipeline/unsup_targets.R
@@ -0,0 +1,29 @@
+# 03_unsup_pipeline/unsup_targets.R
+
+# Load necessary libraries
+library(targets)
+library(tarchetypes)
+library(here)
+
+# Source custom functions and data loading script
+
+# Define the targets for the supervised learning pipeline
+unsup_targets <- list(
+  # # Load data
+  # tar_target(unsup_raw_data, load_data()),
+  # 
+  # # Preprocess data
+  # tar_target(unsup_clean_data, preprocess_data(unsup_raw_data)),
+  # 
+  # # Perform PCA
+  # tar_target(unsup_pca_results, perform_pca(unsup_clean_data)),
+  # 
+  # # Perform clustering
+  # tar_target(unsup_clustering_results, perform_clustering(unsup_clean_data)),
+  # 
+  # # Visualize PCA results
+  # tar_target(unsup_pca_plot, plot_pca(unsup_pca_results)),
+  # 
+  # # Visualize clustering results
+  # tar_target(unsup_clustering_plot, plot_clustering(unsup_clustering_results))
+)
