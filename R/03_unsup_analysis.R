22045eec1ffc09b406bc522fdc3f03ec8f0a835e
Tuan Nguyen <dn370@exeter.ac.uk>
2025-07-17 17:41:51 +0000

    feat: Update analysis with functions to find the best optimal k for kmeans clustering

--- R/03_unsup_analysis.R
+++ R/03_unsup_analysis.R
@@ -160,9 +160,7 @@ for (i in 1:(nrow(gap_df) - 1)) {
     break
   }
 }
-optimal_k # 3
 
-## Choose k = 3 based on elbow plot
 ## Build a function to apply k-means clustering
 unsup_apply_kmeans <- function(pca_data, optimal_k) {
   set.seed(123)
@@ -171,37 +169,59 @@ unsup_apply_kmeans <- function(pca_data, optimal_k) {
   
   ## Plot clusters on PCA components
   pca_data$cluster <- as.factor(km_result$cluster)
-  
-  return(km_result)
+  km_result
 }
 ## Build a function to plot clusters
 unsup_plot_clusters <- function(pca_data, km_result, optimal_k) {
   pca_data$cluster <- as.factor(km_result$cluster)
-  
   library(ggplot2)
   cluster_plot <- ggplot(pca_data, aes(x = PC1, y = PC2, color = cluster)) +
     geom_point(alpha = 0.7, size = 2) +
     labs(title = paste("K-means Clustering with k =", optimal_k), x = "PC1", y = "PC2") +
     theme_minimal() +
     scale_color_brewer(palette = "Set1")
-  print(cluster_plot)
+  cluster_plot
 }
 ## Build a function to calculate silhouette score
 unsup_calculate_silhouette <- function(km_result, pca_data) {
   library(cluster)
   sil_score <- silhouette(km_result$cluster, dist(pca_data[, 1:4]))
   avg_silhouette <- mean(sil_score[, 3])
-  return(avg_silhouette)
+  avg_silhouette
 }
-
+## Build a function to add silhouette score to the data frame
+unsup_add_silhouette_score <- function(silhouette_scores, k, silhouette_score) {
+  silhouette_scores <- rbind(silhouette_scores, data.frame(k = k, silhouette_score = silhouette_score))
+  silhouette_scores
+}
 ## Build a function to choose the best k based on silhouette score
 unsup_choose_best_k <- function(silhouette_scores) {
   best_k <- silhouette_scores[which.max(silhouette_scores$silhouette_score), "k"]
-  return(best_k)
+  best_k
 }
+## Build a function to apply k-means until max_k to find optimal k
+unsup_apply_kmeans_until_optimal <- function(pca_data, max_k) {
+  silhouette_scores <- data.frame(k = integer(), silhouette_score = numeric())
+  
+  for (k in 2:max_k) {
+    km_result <- unsup_apply_kmeans(pca_data, k)
+    avg_silhouette <- unsup_calculate_silhouette(km_result, pca_data)
+    silhouette_scores <- unsup_add_silhouette_score(silhouette_scores, k, avg_silhouette)
+    cat("Silhouette Score for k =", k, ":", avg_silhouette, "\n")
+  }
+  ## Choose the best k based on silhouette scores
+  best_k <- unsup_choose_best_k(silhouette_scores)
+  best_km_result <- unsup_apply_kmeans(pca_data, best_k)
+  best_silhouette_scores <- unsup_calculate_silhouette(best_km_result, pca_data)
+  list(best_k, best_km_result, silhouette_scores)
+}
+
+optimal_k # 3
+
 ## Set a silhouette scores data frame
 silhouette_scores <- data.frame(k = integer(), silhouette_score = numeric())
 
+## Choose k = 3 based on elbow plot and gap statistic
 # Apply k-means clustering with optimal k
 km_result <- unsup_apply_kmeans(pca_data, optimal_k)
 ## Plot clusters on PCA components
@@ -209,7 +229,7 @@ unsup_plot_clusters(pca_data, km_result, optimal_k)
 ## Calculate silhouette score
 silhouette_score <- unsup_calculate_silhouette(km_result, pca_data)
 ## Add silhouette score to the data frame
-silhouette_scores <- rbind(silhouette_scores, data.frame(k = optimal_k, silhouette_score = silhouette_score))
+silhouette_scores <- unsup_add_silhouette_score(silhouette_scores, optimal_k, silhouette_score)
 ## Print silhouette score
 cat("Silhouette Score for k =", optimal_k, ":", silhouette_score, "\n")
 ## Aggregate the data by cluster
@@ -224,7 +244,7 @@ unsup_plot_clusters(pca_data, km_result_k4, optimal_k4)
 ## Calculate silhouette score for k=4
 silhouette_score_k4 <- unsup_calculate_silhouette(km_result_k5, pca_data)
 ## Add silhouette score for k=4 to the data frame
-silhouette_scores <- rbind(silhouette_scores, data.frame(k = optimal_k4, silhouette_score = silhouette_score_k4))
+silhouette_scores <- unsup_add_silhouette_score(silhouette_scores, optimal_k4, silhouette_score_k4)
 cat("Silhouette Score for k =", optimal_k4, ":", silhouette_score_k4, "\n")
 ## Aggregate the data by cluster for k=4
 olive_oil$cluster_k4 <- km_result_k4$cluster
@@ -239,7 +259,7 @@ unsup_plot_clusters(pca_data, km_result_k5, optimal_k5)
 ## Calculate silhouette score for k=5
 silhouette_score_k5 <- unsup_calculate_silhouette(km_result_k5, pca_data)
 ## Add silhouette score for k=5 to the data frame
-silhouette_scores <- rbind(silhouette_scores, data.frame(k = optimal_k5, silhouette_score = silhouette_score_k5))
+silhouette_scores <- unsup_add_silhouette_score(silhouette_scores, optimal_k5, silhouette_score_k5)
 cat("Silhouette Score for k =", optimal_k5, ":", silhouette_score_k5, "\n")
 ## Aggregate the data by cluster for k=5
 olive_oil$cluster_k5 <- km_result_k5$cluster
@@ -254,16 +274,23 @@ unsup_plot_clusters(pca_data, km_result_k6, optimal_k6)
 ## Calculate silhouette score for k=6
 silhouette_score_k6 <- unsup_calculate_silhouette(km_result_k6, pca_data)
 ## Add silhouette score for k=6 to the data frame
-silhouette_scores <- rbind(silhouette_scores, data.frame(k = optimal_k6, silhouette_score = silhouette_score_k6))
+silhouette_scores <- unsup_add_silhouette_score(silhouette_scores, optimal_k6, silhouette_score_k6)
 cat("Silhouette Score for k =", optimal_k6, ":", silhouette_score_k6, "\n")
 ## Aggregate the data by cluster for k=6
 olive_oil$cluster_k6 <- km_result_k6$cluster
 aggregate(. ~ cluster_k6, data = olive_oil[, -1], FUN = mean)
 
-## Loop through silhouette scores for k=3 to k=6 to find best k
-best_k <- unsup_choose_best_k(silhouette_scores)
-# Print the best k with highest silhouette score
+## Use the function to loop through k and apply k-means clustering
+### Get the number of k possible
+max_k <- 10
+### Apply k-means clustering until max_k
+result <- unsup_apply_kmeans_until_optimal(pca_data, max_k)
+best_k <- result[[1]]
+best_km_result <- result[[2]]
+best_silhouette_scores <- result[[3]]
+## Print the best k with silhouette score
 cat("Best k based on silhouette score:", best_k, "\n")
+cat("Best silhouette score:", max(best_silhouette_scores$silhouette_score), "\n")
 # ---
 
 # Apply DBSCAN clustering
