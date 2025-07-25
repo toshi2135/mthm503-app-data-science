6eb0e9a38ae4984bd58577325d0b7397ef40c1ad
Tuan Nguyen <dn370@exeter.ac.uk>
2025-07-25 10:08:07 +0000

    feat: Update pipeline to create final report

--- _targets.R
+++ _targets.R
@@ -21,11 +21,20 @@ if (!is_CI) {
   source(here("R/02_reg_pipeline", "reg_targets.R")) # defines reg_targets
   message("✅ Loading unsupervised learning pipeline")
   source(here("R/03_unsup_pipeline", "unsup_targets.R")) # defines unsup_targets
+  message("✅ Loading final report pipeline")
+  final_report <- tar_render(
+    name = "final_report",
+    path = here("R", "final_report.Rmd"),
+    output_format = "pdf_document",
+    output_file = "final_report.pdf",
+    output_dir = "reports"
+  )
 } else {
   message("⏭️ Skipping pipelines for CI")
   sup_targets <- list() # fallback
   reg_targets <- list() # fallback
   unsup_targets <- list() # fallback
+  final_report <- list() # fallback
 }
 
 # Define the targets pipeline
@@ -49,5 +58,6 @@ list(
   # Production pipelines
   sup_targets,
   reg_targets,
-  unsup_targets
+  unsup_targets,
+  final_report
 )
